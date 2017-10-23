af87b3992491843885e157b0faf62cf26d77676c
Christopher Williams <chris.a.williams@gmail.com>
2017-10-23 13:36:24 -0400

    Bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,7 @@ maintainer_email 'tyler.sanders@geocent.com'
 license          'Apache 2.0'
 description      'Installs/Configures eclipse'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends          "java"
 depends          "ark"
-
